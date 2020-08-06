1bbdfe48ed39b51739389d2f028ebcd1dcb7d317
MATTHEW BAUER <matt@lynch2.com>
2020-08-06 11:55:15 -0500

    First commit

---
+++ app/Http/Controllers/Challenge.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class Challenge extends Controller
+{
+    //
+
+    public function displayContacts(Request $request)
+    {
+          dd("Makes it here....");
+//        $clients = $this->getBillingClientData();
+//
+//        return view('dashboard.billing.clients', [ 'clients' => $clients ]);
+
+    }
+}
